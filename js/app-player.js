f27d8f1c65b5a1e0dff76be122e502593765b6e4
mono <monotom@gmail.com>
2014-02-09 23:26:48 +0100

    add auto completion feature to search input

--- js/app-player.js
+++ js/app-player.js
@@ -62,6 +62,35 @@ var AppPlayer = function(id, config){
     $('#search-'+id+'-button').trigger('click');
   });
 
+  //search autocompleetion
+  $('#search-'+id+'-input').autocomplete({
+        source: function(request, response) {
+            $.getJSON("http://suggestqueries.google.com/complete/search?callback=?",
+                {
+                  "hl":"en", // Language
+                  "ds":"yt", // Restrict lookup to youtube
+                  "jsonp":"suggestCallBack", // jsonp callback function name
+                  "q":request.term, // query term
+                  "client":"youtube" // force youtube style response, i.e. jsonp
+                }
+            );
+            suggestCallBack = function (data) {
+
+                var suggestions = [];
+                $.each(data[1], function(key, val) {
+                    suggestions.push({"value":val[0]});
+                });
+                suggestions.length = 5; // prune suggestions list to only 5 items
+
+                try{
+                  response(suggestions);
+                }
+                catch(e){
+                  //prevent jquery bug, when autocompleete source returns undefined
+                };
+            };
+        },
+    });
 
   //clickable result rows
   $(document).on('click', '#search-'+id+'-result .record', function(){
